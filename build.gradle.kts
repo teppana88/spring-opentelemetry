9711802112e2be1b052e054cedb6e18a43aad244
Teemu Piirainen <teemu.piirainen@awave.fi>
2024-12-09 10:41:11 +0200

    Basic observability implementation
    Opentelemetry, collector, Grafana, Tempo, Loki, Prometheus

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,8 @@
+import org.springframework.boot.gradle.plugin.SpringBootPlugin
+
 plugins {
-	kotlin("jvm") version "1.9.25"
-	kotlin("plugin.spring") version "1.9.25"
+	kotlin("jvm") version "2.1.0"
+	kotlin("plugin.spring") version "2.1.0"
 	war
 	id("org.springframework.boot") version "3.4.0"
 	id("io.spring.dependency-management") version "1.1.6"
@@ -20,6 +22,7 @@ repositories {
 }
 
 dependencies {
+	// These are basic Spring Boot dependencies
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
@@ -27,6 +30,19 @@ dependencies {
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
+	// These are opentelemetry dependencies
+	// For Spring Boot native - OpenTelemetry core instrumentation for Spring Boot
+	implementation(platform(SpringBootPlugin.BOM_COORDINATES))
+	implementation(platform("io.opentelemetry.instrumentation:opentelemetry-instrumentation-bom:2.10.0"))
+	implementation("io.opentelemetry.instrumentation:opentelemetry-spring-boot-starter:2.10.0")
+	// Enable metrics
+	implementation("org.springframework.boot:spring-boot-starter-actuator")
+	// enable tracing bridge so that traceID is propagated
+	implementation("io.micrometer:micrometer-tracing-bridge-otel")
+	// DB traces for monitoring (not used in this example but if you want to monitor DB)
+//	implementation("net.ttddyy.observation:datasource-micrometer-spring-boot:1.0.5")
+
 }
 
 kotlin {
