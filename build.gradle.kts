ef868401b0036aea0a59aa3ee0c2da5bb02773c7
Teemu Piirainen <teemu.piirainen@awave.fi>
2024-12-30 06:53:56 +0200

    Dependency updates

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,8 +4,8 @@ plugins {
 	kotlin("jvm") version "2.1.0"
 	kotlin("plugin.spring") version "2.1.0"
 	war
-	id("org.springframework.boot") version "3.4.0"
-	id("io.spring.dependency-management") version "1.1.6"
+	id("org.springframework.boot") version "3.4.1"
+	id("io.spring.dependency-management") version "1.1.7"
 }
 
 group = "fi.awave"
@@ -34,8 +34,8 @@ dependencies {
 	// These are opentelemetry dependencies
 	// For Spring Boot native - OpenTelemetry core instrumentation for Spring Boot
 	implementation(platform(SpringBootPlugin.BOM_COORDINATES))
-	implementation(platform("io.opentelemetry.instrumentation:opentelemetry-instrumentation-bom:2.10.0"))
-	implementation("io.opentelemetry.instrumentation:opentelemetry-spring-boot-starter:2.10.0")
+	implementation(platform("io.opentelemetry.instrumentation:opentelemetry-instrumentation-bom:2.11.0"))
+	implementation("io.opentelemetry.instrumentation:opentelemetry-spring-boot-starter:2.11.0")
 	// Enable metrics
 	implementation("org.springframework.boot:spring-boot-starter-actuator")
 	// enable tracing bridge so that traceID is propagated
